43288c18ddb15ccd48a10c362962ab92ceaefaf8
Luca Gutzeit <luca.gutzeit@gmx.de>
2020-05-11 00:50:29 +0200

    Added description to the timer

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("May 11, 2020 10:00:00").getTime(),
+let countDown = new Date("May 11, 2020 12:00:00").getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
 
-    /* document.getElementById('days').innerText = Math.floor(distance / (day)) */
+    document.getElementById("days").innerText = Math.floor(distance / day);
 
     document.getElementById("hours").innerText = Math.floor(
       (distance % day) / hour
